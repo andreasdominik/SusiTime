4da2ae923ca536cf772c5051badcecda158a7f09
Andreas Dominik <andreas.dominik@mni.thm.de>
2019-03-20 16:42:15 +0100

    some docu added

--- App/skill-actions.jl
+++ App/skill-actions.jl
@@ -4,6 +4,12 @@
 #
 # ... and link the function with the intent name as shown in config.jl
 #
+# The functions will be called by the main callback function with
+# 2 arguments:
+# * MQTT-Topic as String
+# * MQTT-Payload (The JSON part) as a nested dictionary, with all keys
+#   as Symbols (Julia-style)
+#
 """
 function powerOnMyDevice(topic, payload)
 
