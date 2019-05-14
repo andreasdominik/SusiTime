8abfbb64fd9035478bd357bf1984d3d9d5afe940
andreasdominik <andreas.dominik@mni.thm.de>
2019-05-14 22:29:13 +0200

    naming convention ...

--- Skill/exported.jl
+++ Skill/exported.jl
@@ -13,7 +13,7 @@ end
 # This function is executed to run a
 # skill action in the module.
 #
-function callbackRun(fun, intent, payload)
+function callbackRun(fun, topic, payload)
 
     Snips.setSiteId(payload[:siteId])
     Snips.setSessionId(payload[:sessionId])
