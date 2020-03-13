4bb5f5d4e911cb703fb1337fdccd59e64b8c5231
andreas.dominik <andreas.dominik@mni.thm.de>
2020-03-13 17:30:37 +0100

    languages fixed

--- Skill/languages.jl
+++ Skill/languages.jl
@@ -2,8 +2,8 @@ Snips.addText("de", :isay, ["Es ist",
                             "Es ist gerade",
                             "Es ist jetzt",
                             "Jetzt ist es",
-                            "Die aktuelle Zeit ist"]
+                            "Die aktuelle Zeit ist"])
 
 Snips.addText("en", :isay, ["It is",
                             "The time is",
-                            "It is now"]
+                            "It is now"])
